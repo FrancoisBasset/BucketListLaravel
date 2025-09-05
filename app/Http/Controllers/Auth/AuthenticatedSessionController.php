806ab90df1a872fa8da442ddc1f1198a856a2e96
FrancoisBasset <mrfrancoisbasset@gmail.com>
2025-09-05 15:19:35 +0200

    Format files

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,37 +2,33 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
-class AuthenticatedSessionController extends Controller
-{
-    /**
-     * Handle an incoming authentication request.
-     */
-    public function store(LoginRequest $request): Response
-    {
-        $request->authenticate();
+class AuthenticatedSessionController {
+	/**
+	 * Handle an incoming authentication request.
+	 */
+	public function store(LoginRequest $request): Response {
+		$request->authenticate();
 
-        $request->session()->regenerate();
+		$request->session()->regenerate();
 
-        return response()->noContent();
-    }
+		return response()->noContent();
+	}
 
-    /**
-     * Destroy an authenticated session.
-     */
-    public function destroy(Request $request): Response
-    {
-        Auth::guard('web')->logout();
+	/**
+	 * Destroy an authenticated session.
+	 */
+	public function destroy(Request $request): Response {
+		Auth::guard('web')->logout();
 
-        $request->session()->invalidate();
+		$request->session()->invalidate();
 
-        $request->session()->regenerateToken();
+		$request->session()->regenerateToken();
 
-        return response()->noContent();
-    }
+		return response()->noContent();
+	}
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -9,25 +9,23 @@ use Illuminate\Support\Str;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
-class UserFactory extends Factory
-{
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
+class UserFactory extends Factory {
+	/**
+	 * The current password being used by the factory.
+	 */
+	protected static ?string $password;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
-        ];
-    }
+	/**
+	 * Define the model's default state.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function definition(): array {
+		return [
+			'name' => fake()->name(),
+			'email' => fake()->unique()->safeEmail(),
+			'password' => static::$password ??= Hash::make('password'),
+			'remember_token' => Str::random(10),
+		];
+	}
 }
