56c364a9e8b4509f5a6e1a6f2ef34943b54c92fd
FrancoisBasset <mrfrancoisbasset@gmail.com>
2025-09-08 11:13:00 +0200

    Login Route

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Support\Facades\Hash;
+
 class LoginController {
-    //
+    public function login(LoginRequest $request) {
+        $user = User::where('username', $request->username)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw new AuthenticationException('Les identifiants sont incorrects');
+        }
+
+        // Création du token Sanctum
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
 }
